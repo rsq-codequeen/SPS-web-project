7ff8cdd892dc7535470f8c80831b31a4ed923a67
rsq-codequeen <159681694+rsq-codequeen@users.noreply.github.com>
2025-09-19 00:21:47 +0500

    deleted everything

--- SPS-web/src/App.jsx
+++ SPS-web/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import Hero from './components/home/hero';
-import Section2 from './components/home/section2';
-import Section3 from './components/home/section3';
-import Section4 from './components/home/section4';
-
 function App() {
   return (
     <>
-   <Hero/>
-   <Section2/>
-   <Section3/>
-   <Section4/>
+ 
  
    </>
   )
