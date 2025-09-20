e7b035281a268bac77337e3d88a40b5c8ee6e2a2
rsq-codequeen <159681694+rsq-codequeen@users.noreply.github.com>
2025-09-20 16:58:25 +0500

    made tech partners

--- SPS-web/src/App.jsx
+++ SPS-web/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import HomeHero from './sections/Home_hero'
 import MaindomaisSection from './sections/MainDomainSection'
 import Startup from './sections/startup'
+import TechPartners from './sections/TechPartners';
 function App() {
   return (
     <div className="App">
-      <HomeHero />
-     <MaindomaisSection/>
-      <Startup/>
-
+    <HomeHero />
+    <MaindomaisSection/>
+    <Startup/>
+    <TechPartners/>
     </div>
   )
 }
