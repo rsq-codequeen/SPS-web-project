f37fb179ec08207c2a009c07fb2d4117bb78933b
Muaz Nadeem <153768999+muaz-nadeem@users.noreply.github.com>
2025-09-20 18:04:58 +0500

    made applications section

--- SPS-web/src/App.jsx
+++ SPS-web/src/App.jsx
@@ -3,12 +3,14 @@ import HomeHero from './sections/Home_hero'
 import MaindomaisSection from './sections/MainDomainSection'
 import Startup from './sections/startup'
 import TechPartners from './sections/TechPartners';
+import Applications from './Components/Applications';
 function App() {
   return (
     <div className="App">
     <HomeHero />
     <MaindomaisSection/>
     <Startup/>
+    <Applications/>
     <TechPartners/>
     </div>
   )
